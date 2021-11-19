85dbf0ebfe792b44832871164a72c734e39506a9
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-11-19 15:56:43 -0300

    Blade - Template com extends, section e yield parte 1

---
+++ resources/views/site/layouts/basico.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Super Gestão - Sobre Nós</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/estilo_basico.css') }}">
+    </head>
+
+    <body>
+      @yield('conteudo')
+    </body>
+</html>
