634c358c1ffcb7dff5f3c942cc2c19f87abba93d
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-11-19 10:12:14 -0300

    Implementando, agrupando e nomeando as rotas

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
 
-Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', [\App\Http\Controllers\LoginController::class, 'login'])->name('site.login');
 
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+Route::prefix('/app')->group(function() {
+Route::get('/clientes', function() {return 'clientes';})->name('app.clientes');
+Route::get('/fornecedores', function() {return 'fornecedores';})->name('app.fornecedores');
+Route::get('/produtos', function() {return 'produtos';})->name('app.produtos');
+});
+
