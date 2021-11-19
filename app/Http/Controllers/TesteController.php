e8f1bc361b63adfd501565e1a99fa5904cfc5aba
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-11-19 10:43:41 -0300

    Encaminhando parâmetros do controlador para visualização

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -8,6 +8,18 @@ class TesteController extends Controller
 {
     public function teste(int $p1, int $p2)
     {
-        return "a soma de $p1 + $p2 é: ".($p1+$p2);
+      // echo "a soma de $p1 + $p2 é: ".($p1+$p2);
+
+      //array associativo, manda o indice para a view
+      return view('site.teste', ['p1' => $p1, 'p2' => $p2]); // X é o indice do array
+
+      //compact, é o mais utilizado
+
+      return view('site.teste', compact('p1', 'p2'));
+
+      //with
+
+      // return view('site.teste')->with('xyz' => $p1)->with( 'zzz' => $p2);
+
     }
 }
