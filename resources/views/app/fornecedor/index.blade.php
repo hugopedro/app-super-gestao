f4c4a9de23f05abc53f1594248ff66773c0b3841
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-11-19 11:54:45 -0300

    Blade empty

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -2,15 +2,35 @@
 
 @php
 
-/* if(isset($variavel)) {} retornar true se a variavel estiver definida
-repare que o isset nao se preocupa com o valor da variavel, só quer saber se existe ou n */
-  
+/*
+
+if(empty($variavel)) {} retornar true se a variavel estiver vazia
+
+Nessas situações o PHP entende que a variavel contem um valor vazio:
+
+- ''
+- 0
+- 0.0
+- '0'
+- null
+- false
+- array()
+- $var  (declarar variavel sem atribuir valor)
+
+Ou seja, o empty retorna true nessas situações
+
+00 não é vazio
+
+*/
 @endphp
 
-Fornecedor: {{ $fornecedores[1]['nome'] }}
+Fornecedor: {{ $fornecedores[0]['nome'] }}
 <br />
-Status: {{ $fornecedores[1]['status'] }}
+Status: {{ $fornecedores[0]['status'] }}
 <br />
-@isset($fornecedores[1]['cnpj'])
-  CNPJ: {{ $fornecedores[1]['cnpj'] }}
+@isset($fornecedores[0]['cnpj'])
+  CNPJ: {{ $fornecedores[0]['cnpj'] }}
+  @empty($fornecedores[0]['cnpj'])
+    - Vazio
+  @endempty
 @endisset
