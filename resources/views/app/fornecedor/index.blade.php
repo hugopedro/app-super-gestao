65e10317609e6434f6cf8954f18db56872b329d1
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-11-19 14:24:35 -0300

    Blade - For

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -5,25 +5,14 @@
 @endphp
 
 @isset($fornecedores)
-Fornecedor: {{ $fornecedores[0]['nome'] }}
-<br />
-Status: {{ $fornecedores[0]['status'] }}
-<br />
-CNPJ: {{ $fornecedores[0]['cnpj'] ?? 'Dado não foi preenchido' }}
-<br />
-Telefone: ({{ $fornecedores[0]['ddd'] ?? '' }} {{ $fornecedores[0]['telefone'] ?? '' }})
-@switch($fornecedores[0]['ddd'])
-  @case('01')
-        <br />
-        São Paulo - SP
-    @break
-  @case('32')
-        Juiz de Fora - MG
-    @break
-  @case('85')
-        Fortaleza - CE
-    @break
-  @default
-    Estado desconhecido
-@endswitch
+  @for($i=0; isset($fornecedores[$i]); $i++)
+    Fornecedor: {{ $fornecedores[$i]['nome'] }}
+    <br />
+    Status: {{ $fornecedores[$i]['status'] }}
+    <br />
+    CNPJ: {{ $fornecedores[$i]['cnpj'] ?? 'Dado não foi preenchido' }}
+    <br />
+    Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }} {{ $fornecedores[$i]['telefone'] ?? '' }})
+    <hr>
+  @endfor
 @endisset
