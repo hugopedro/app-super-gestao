486fb0694cba0d86f42f2dc6a50125572083a141
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-11-19 11:26:46 -0300

    Blade unless

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,13 +1,23 @@
 <h3>Fornecedor</h3>
 
 @php
+
+/* if(condicao) {} executa se a condicao for verdadeira 
+já o unless executa se o retorno for falso */
   
 @endphp
 
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
-  <h3>Existem alguns Fornecedores cadastrados</h3>
-@elseif(count($fornecedores) > 10)
-  <h3>Existem muitos Fornecedores cadastrados</h3>
-@else
-  <h3>Não existem Fornecedores cadastrados</h3>
+Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br />
+Status: {{ $fornecedores[0]['status'] }}
+<br />
+@if(!($fornecedores[0]['status'] == 'S'))
+  Fornecedor Inativo
 @endif
+<br>
+@unless($fornecedores[0]['status'] == 'S') <!-- executa se o retorno da condicao for false -->
+  Fornecedor Inativo
+@endunless
+<br>
+
+{{-- Ou seja, @endunless é igual ao if com !, é preferível usar o @unless--}}
