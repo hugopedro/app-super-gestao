4674b538864491bfc2dfb3b14d39b16a46aae049
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-11-19 14:27:13 -0300

    Blade - while

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -5,14 +5,18 @@
 @endphp
 
 @isset($fornecedores)
-  @for($i=0; isset($fornecedores[$i]); $i++)
-    Fornecedor: {{ $fornecedores[$i]['nome'] }}
-    <br />
-    Status: {{ $fornecedores[$i]['status'] }}
-    <br />
-    CNPJ: {{ $fornecedores[$i]['cnpj'] ?? 'Dado não foi preenchido' }}
-    <br />
-    Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }} {{ $fornecedores[$i]['telefone'] ?? '' }})
+    @php $i = 0; @endphp
+    @while(isset($fornecedores[$i]))
+      Fornecedor: {{ $fornecedores[$i]['nome'] }}
+      <br />
+      Status: {{ $fornecedores[$i]['status'] }}
+      <br />
+      CNPJ: {{ $fornecedores[$i]['cnpj'] ?? 'Dado não foi preenchido' }}
+      <br />
+      Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }} {{ $fornecedores[$i]['telefone'] ?? '' }})
+      @php
+        $i++;
+      @endphp
     <hr>
-  @endfor
+    @endwhile
 @endisset
