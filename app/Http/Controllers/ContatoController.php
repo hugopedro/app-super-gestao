ebe2e8f742c6463e3a010bb1d6ad021e94c75217
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-11-19 09:44:17 -0300

    Super Gestão - Criando o menu de navegação

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -8,6 +8,6 @@ class ContatoController extends Controller
 {
     public function contato()
     {
-      view('site.contato');
+      return view('site.contato');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,3 @@ Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
 
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
-
-//nome, categoria, assunto, mensagem
-
-Route::get('/contato/{nome}/{categoria_id}', 
-function(
-    string $nome= 'Desconhecido',
-    int $categoria_id = 1 // 1 - Informação
-    ) {
-      return "Olá {$nome} - {$categoria_id}"; 
-    })->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
-    // protege que o nome seja apenas letras e que a categoria seja apenas números
