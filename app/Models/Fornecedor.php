dcfbe448eca10f4e56c456dfa52dca6893818f8c
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-11-22 15:55:16 -0300

    Migrations, Eloquent, Tinker

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Fornecedor extends Model
 {
     use HasFactory;
+
+    /* o laravel adiciona s na tabela automaticamente para ficar no plural
+    mas nem sempre isso funciona, no caso ficaria "fornecedors", então
+    é necessário alterar o nome da tabela manualmente no plural para não dar erro */
+
+    protected $table = 'fornecedores';
 }
