00e3f12fac07be16702c49dce50fc3dbf226d3e1
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-11-23 16:17:07 -0300

    Formulários - Gravando os dados do formulário no banco de dados

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato()
-    {
-      var_dump($_POST);
-      return view('site.contato', ['titulo' => 'Contato(teste)']);
+    public function contato(Request $request) {
+        $contato = new SiteContato();
+        $contato->create($request->all()); // salva no banco
+        //print_r($contato->getAttributes());
+        return view('site.contato', ['titulo' => 'Contato(teste)']);
     }
 }
