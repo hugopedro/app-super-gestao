fae0c968d976c2d73350afa8102feab9f964bd66
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-11-24 13:59:48 -0300

    Criando o model LogAcesso e sua migration

--- app/Http/Middleware/LogAcessoMiddleware.php
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -3,6 +3,8 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use \App\Models\LogAcesso;
+
 use Illuminate\Http\Request;
 
 class LogAcessoMiddleware
@@ -20,6 +22,9 @@ class LogAcessoMiddleware
         //return $next($request);
         //response  - manipular
         //Só pra mostrar que tá passando pelo middleware
+        $ip = $request->server->get('REMOTE_ADDR');
+        $rota = $request->getRequestUri();
+        LogAcesso::create(['log' => "IP $ip requisitou a rota $rota"]);
         return Response('Chegamos ao middleware e finalizamos no proprio middleware');
     }
 }
