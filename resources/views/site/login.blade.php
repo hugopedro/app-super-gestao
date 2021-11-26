7068a730c3ae9bdea7cf0e2de898228f803ac69b
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-11-26 09:58:52 -0300

    Recebendo os parâmetros de usuário e senha

--- resources/views/site/login.blade.php
+++ resources/views/site/login.blade.php
@@ -11,15 +11,19 @@
       </div>
 
       <div class="informacao-pagina">
-        <div style="width:30%; margin-left: auto; margin-right:auto;">
-          <form action={{ route('site.login') }} method="post">
-            @csrf
-            <input name="usuario" type="text" placeholder="Usuário" class="borda-preta">
-            <input name="senha" type="password" placeholder="Senha" class="borda-preta">
-            <button type="submit" class="borda-preta">Acessar </button>
-          </form>
+        <div style="width:30%; margin-left: auto; margin-right: auto;">
+            <form action={{ route('site.login') }} method="post">
+                @csrf
+                <input name="usuario" value="{{ old('usuario') }}" type="text" placeholder="Usuário" class="borda-preta">
+                {{ $errors->has('usuario') ? $errors->first('usuario') : '' }}
+
+                <input name="senha" value="{{ old('senha') }}" type="password" placeholder="Senha" class="borda-preta">
+                {{ $errors->has('senha') ? $errors->first('senha') : '' }}
+
+                <button type="submit" class="borda-preta">Acessar</button>
+            </form>
         </div>
-      </div>  
+    </div>  
   </div>
 
   <div class="rodape">
