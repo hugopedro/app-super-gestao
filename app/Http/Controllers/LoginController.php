2d981a0f4624f6b126a3ee5c30680e86efa503d7
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-11-26 11:30:13 -0300

    Adicionando a função logout

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -63,6 +63,7 @@ class LoginController extends Controller
     }
 
     public function sair() {
-      echo 'sair';
+      session_destroy();
+      return redirect()->route('site.index');
     }
 }
