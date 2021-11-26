fbc332822935b46789cd6a6671e1408e2e6cbdcc
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-11-26 10:27:09 -0300

    Redirect com envio de parâmetros - Apresentando mensagem de erro de login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,8 +7,12 @@ use App\Models\User;
 
 class LoginController extends Controller
 {
-    public function index() {
-      return view('site.login', ['titulo' => 'Login']);
+    public function index(Request $request) {
+      $erro = '';
+      if ($request->get('erro') == 1) {
+        $erro = 'Usuário ou senha inválidos';
+      }
+      return view('site.login', ['titulo' => 'Login', 'erro' => $erro]);
     }
 
     public function autenticar(Request $request) {
@@ -44,8 +48,7 @@ class LoginController extends Controller
         echo "Usuário encontrado";
         echo "<br>";
       } else {
-        echo "Usuário não encontrado";
-        echo "<br>";
+        return redirect()->route('site.login', ['erro' => 1]);
       }
 
       echo '<pre>';
--- resources/views/site/login.blade.php
+++ resources/views/site/login.blade.php
@@ -22,6 +22,7 @@
 
                 <button type="submit" class="borda-preta">Acessar</button>
             </form>
+            {{ isset($erro) && $erro != '' ? $erro : '' }}
         </div>
     </div>  
   </div>
