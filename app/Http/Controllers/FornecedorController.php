a9f22fba9c312dda54ac7043fe590977a737b627
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-11-26 15:06:57 -0300

    Implementando o cadastro de fornecedores parte 4 (atualização de registros)

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -26,7 +26,8 @@ class FornecedorController extends Controller
 
         $msg = '';
 
-        if($request->input('_token') != '') {
+        //inclusão
+        if($request->input('_token') != '' && $request->input('id') == '') {
             //validacao
             $regras = [
                 'nome' => 'required|min:3|max:40',
@@ -54,6 +55,28 @@ class FornecedorController extends Controller
             //dados view
             $msg = 'Cadastro realizado com sucesso';
         }
-        return view('app.fornecedor.adicionar', ['msg' => $msg]);
+
+        //edição
+        if($request->input('_token') != '' && $request->input('id') != '') {
+          $fornecedor = Fornecedor::find($request->input('id'));
+          $update = $fornecedor->update($request->all());
+
+          if($update) {
+              $msg = 'Atualização realizada com sucesso';
+          } else {
+              $msg = 'Erro ao tentar atualizar o registro';
+          }
+
+          return redirect()->route('app.fornecedor.editar', ['id' => $request->input('id'), 'msg' => $msg]);
+      }
+
+      return view('app.fornecedor.adicionar', ['msg' => $msg]);
     }
+
+    public function editar($id, $msg = '') { // já que a msg é opcionar define-se '' como default
+
+      $fornecedor = Fornecedor::find($id);
+
+      return view('app.fornecedor.adicionar', ['fornecedor' => $fornecedor, 'msg' => $msg]);
+  }
 }
