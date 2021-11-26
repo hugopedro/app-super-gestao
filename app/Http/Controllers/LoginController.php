74fa378a5e948830f5d3cbf8a627dad33cf2493a
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-11-26 10:51:16 -0300

    Iniciando a Superglobal Session e validando o acesso a rotas protegidas

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -12,6 +12,9 @@ class LoginController extends Controller
       if ($request->get('erro') == 1) {
         $erro = 'Usuário ou senha inválidos';
       }
+      if ($request->get('erro') == 2) {
+        $erro = 'Você precisa estar logado para acessar esta página';
+      }
       return view('site.login', ['titulo' => 'Login', 'erro' => $erro]);
     }
 
@@ -36,17 +39,18 @@ class LoginController extends Controller
       $email = $request->get('usuario');
       $password = $request->get('senha');
 
-      echo "Usuário $email senha $password";
-      echo "<br>";
-
       // iniciar o Model User
       $user = new User();
 
       $usuario = $user->where('email', $email)->where('password', $password)->get()->first();
 
       if(isset($usuario->name)) { // see existe um atributo name com valor tal no objeto user
-        echo "Usuário encontrado";
-        echo "<br>";
+        session_start();
+
+        $_SESSION['nome'] = $usuario->name;
+        $_SESSION['email'] = $usuario->email;
+        
+        return redirect()->route('app.clientes');
       } else {
         return redirect()->route('site.login', ['erro' => 1]);
       }
